ab120a96e2a20f015d81d094a105a6495edaea46
Nicolas Husser <nicolas.husser@gmail.com>
2014-07-05 15:50:18 -0400

    Importing old website code I made for template

---
+++ css/config.rb
@@ -0,0 +1,9 @@
+# Require any addition compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "/"
+sass_dir = "/"
+images_dir = "/images"
+javascripts_dir = "/"
+# To enable relative paths to assests via compass helper
+# relative_assests = true
---
+++ js/app.js
@@ -0,0 +1,55 @@
+// create the module and name it scotchApp
+var app = angular.module('app', ['ngRoute']);
+
+// configure our routes
+app.config(function($routeProvider) {
+	$routeProvider
+
+		// route for the home page
+		.when('/', {
+			templateUrl : 'pages/home.html',
+			controller  : 'homeController'
+		})
+
+		// route for the about page
+		.when('/about', {
+			templateUrl : 'pages/about.html',
+			controller  : 'aboutController'
+		})
+
+		// route for the contact page
+		.when('/contact', {
+			templateUrl : 'pages/contact.html',
+			controller  : 'contactController'
+		});
+});
+
+
+
+
+// create the controller and inject Angular's $scope
+app.controller('homeController', function($scope) {
+	$scope.videos = [
+		{"url": "images/thumb_sochi.jpg",
+		 "name":"Sochi"},
+		{"url": "images/thumb_sochi.jpg",
+		 "name":"Sochi"},
+		{"url": "images/thumb_sochi.jpg",
+		 "name":"Sochi"}
+	]
+
+});
+
+
+
+
+app.controller('aboutController', function($scope) {
+	$scope.message = 'Look! I am an about page.';
+});
+
+
+
+
+app.controller('contactController', function($scope) {
+	$scope.message = 'Contact us! JK. This is just a demo.';
+});
