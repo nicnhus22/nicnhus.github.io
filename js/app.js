1d25275dd22bb47939e673862422ef3059627dee
Nicolas Husser <nicolas.husser@free.fr>
2014-10-19 15:18:36 -0400

    Fix typo

--- js/app.js
+++ js/app.js
@@ -32,16 +32,16 @@ app.controller('homeController', function($scope) {
 	$scope.videos = [
 		{"img": "images/thumb_brainwash.jpg",
 		 "name":"BrainWash",
-		 "url":"http://www.biketrials.tv/fr/video/1245"},
-		 "size":"12"
+		 "url":"http://www.biketrials.tv/fr/video/1245",
+		 "size":"12"}
 		{"img": "images/thumb_fantin.jpg",
 		 "name":"Sochi",
-		 "url":"http://www.biketrials.tv/fr/video/1245"},
-		 "size":"6"
+		 "url":"http://www.biketrials.tv/fr/video/1245",
+		 "size":"6"}
 		{"img": "images/thumb_tourmente.jpg",
 		 "name":"Sochi",
 		 "url":"http://www.biketrials.tv/fr/video/1245"},
-		 "size":"6"
+		 "size":"6"}
 		{"img": "images/thumb_winter_game.jpg",
 		 "name":"Sochi",
 		 "url":"http://www.biketrials.tv/fr/video/1245",
