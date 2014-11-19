f283eb2748acf55c8600036beca76352bd4b618a
Nicolas Husser <nicolas.husser@gmail.com>
2014-11-19 14:32:00 -0500

    Custom directive to load image

--- js/app.js
+++ js/app.js
@@ -99,7 +99,25 @@ app.controller('homeController', function($scope) {
 
 });
 
-
+app.directive('imageonload', function() {
+    return {
+        restrict: 'A',
+      
+        link: function(scope, element) {
+          element.on('load', function() {
+            // Set visibility: true + remove spinner overlay
+              element.removeClass('spinner-hide');
+              element.addClass('spinner-show');
+              element.parent().find('span').remove();
+          });
+          scope.$watch('ngSrc', function() {
+            // Set visibility: false + inject temporary spinner overlay
+              element.addClass('spinner-hide');
+              // element.parent().append('<span class="spinner"></span>');
+          });
+        }
+    };
+});
 
 app.controller('myLifeController', function($scope) {
 });
